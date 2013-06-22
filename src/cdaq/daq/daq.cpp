c846af4797b41a8c737d5498d819fe4e2900b59d
Matti Jukola <matti.jukola@pixact.fi>
2013-06-22 12:49:35 +0300

    Fix indentation

--- src/cdaq/daq/daq.cpp
+++ src/cdaq/daq/daq.cpp
@@ -7,17 +7,17 @@ using boost::asio::detail::socket_ops::network_to_host_short;
 using boost::asio::detail::socket_ops::host_to_network_short;
 
 Daq::Daq(const std::string &com_port, const int &hz)
-	:
-	device_string_(com_port),
-	hz_(hz),
-	device_(io_service_)
+    :
+    device_string_(com_port),
+    hz_(hz),
+    device_(io_service_)
 {
-	
+    
 }
 
 bool Daq::Open()
 {
-	return true;
+    return true;
 }
 
 void Daq::Close()
@@ -26,49 +26,49 @@ void Daq::Close()
 
 void Daq::StartCapturing()
 {
-	StopCapturing();
-	
-	
-	
-	//Clear captured values and raw buffer, this is the "time" for the
-	//first captured value.
-	captured_values_.clear();
-	raw_buffer_.clear();
-	date_begin_ = Date::Now();
-	
-	//Create capturing thread
+    StopCapturing();
+    
+    
+    
+    //Clear captured values and raw buffer, this is the "time" for the
+    //first captured value.
+    captured_values_.clear();
+    raw_buffer_.clear();
+    date_begin_ = Date::Now();
+    
+    //Create capturing thread
 }
 
 void Daq::StopCapturing()
 {
-	stop_capturing_ = true;
-	acquisition_thread_.join();
+    stop_capturing_ = true;
+    acquisition_thread_.join();
 }
 
 void Daq::CapturingLoop()
 {
-	while (!stop_capturing_) {
-		FillRawBuffer();
-		date_end_ = Date::Now();
-	}
+    while (!stop_capturing_) {
+        FillRawBuffer();
+        date_end_ = Date::Now();
+    }
 }
 
 void Daq::GetBufferedData(std::vector<ElementType> *values, Date *begin, Date *end)
 {
-	if (NULL == values || NULL == begin || NULL == end) {
-		//Can not do anything
-		return;
-	}
-	
-	boost::mutex::scoped_lock lock(mutex_data_);
-	*begin = date_begin_;
-	*end = date_end_;
-	*values = captured_values_;
+    if (NULL == values || NULL == begin || NULL == end) {
+        //Can not do anything
+        return;
+    }
+    
+    boost::mutex::scoped_lock lock(mutex_data_);
+    *begin = date_begin_;
+    *end = date_end_;
+    *values = captured_values_;
 }
-	
+    
 size_t Daq::NumberOfBufferedSamples() const
 {
-	return captured_values_.size();
+    return captured_values_.size();
 }
 
 size_t BytesWaiting()
@@ -86,13 +86,13 @@ boost::shared_array<boost::uint8_t> Daq::GetRawSerialData(boost::uint32_t *bytes
 
 void Daq::FillRawBuffer()
 {
-	//Fetch new data from the serial port
-	boost::uint32_t bytes = 0;
-	
-	boost::shared_array<boost::uint8_t> buf = GetRawSerialData(&bytes);
-	for (boost::uint32_t ii = 0; ii < bytes; ++ii) {
-		raw_buffer_.push_back(buf[ii]);
-	}
+    //Fetch new data from the serial port
+    boost::uint32_t bytes = 0;
+    
+    boost::shared_array<boost::uint8_t> buf = GetRawSerialData(&bytes);
+    for (boost::uint32_t ii = 0; ii < bytes; ++ii) {
+        raw_buffer_.push_back(buf[ii]);
+    }
 }
 
 
--- src/cdaq/daq/daq.hh
+++ src/cdaq/daq/daq.hh
@@ -28,99 +28,99 @@
 
 
 namespace cdaq {
-	
+    
 class CDAQDAQAPI Daq
 {
  public:
-	/// Create daq object and set connection parameters
-	/// \param[in] com_port Serial port which to connect
-	/// \param[in] hz Sampling rate which will be set to the daq
-	Daq(const std::string &com_port, const int &hz);
+    /// Create daq object and set connection parameters
+    /// \param[in] com_port Serial port which to connect
+    /// \param[in] hz Sampling rate which will be set to the daq
+    Daq(const std::string &com_port, const int &hz);
 
-	/// Connect to daq and start capturing.
-	/// \return True if conncetion to the daq was successfull
-	// Also starts the capturing thread
-	bool Open();
-	
-	/// Stop capturing
-	void Close();
+    /// Connect to daq and start capturing.
+    /// \return True if conncetion to the daq was successfull
+    // Also starts the capturing thread
+    bool Open();
+    
+    /// Stop capturing
+    void Close();
  
-	/// How many channels this daq has
-	static const int kNumberOfChannels = 8;
-	
-	/// Type of the captured values
-	typedef boost::uint16_t ElementType;
-	
-	/// Type of single sample
-	typedef ElementType SampleType[kNumberOfChannels];
-	
-	/// Get captured data. May return empty vectors and undefined dates
-	/// \param[out] values Captured values
-	/// \param[out] begin Date of the first returned value
-	/// \param[out] end Date of the last returned value
-	void GetBufferedData(std::vector<ElementType> *values, Date *begin, Date *end);
-	
-	/// \return Number of captured  samples
-	size_t NumberOfBufferedSamples() const;
+    /// How many channels this daq has
+    static const int kNumberOfChannels = 8;
+    
+    /// Type of the captured values
+    typedef boost::uint16_t ElementType;
+    
+    /// Type of single sample
+    typedef ElementType SampleType[kNumberOfChannels];
+    
+    /// Get captured data. May return empty vectors and undefined dates
+    /// \param[out] values Captured values
+    /// \param[out] begin Date of the first returned value
+    /// \param[out] end Date of the last returned value
+    void GetBufferedData(std::vector<ElementType> *values, Date *begin, Date *end);
+    
+    /// \return Number of captured  samples
+    size_t NumberOfBufferedSamples() const;
  private:
-	/// Start capturing. Creates capturing thread
-	void StartCapturing();
-	
-	/// Stops capturing. Capturing thread will be destroyed.
-	void StopCapturing();
+    /// Start capturing. Creates capturing thread
+    void StartCapturing();
+    
+    /// Stops capturing. Capturing thread will be destroyed.
+    void StopCapturing();
  
-	/// Data points are captured  in this function. Function should be
-	/// called only by the capturing thread.
-	void CapturingLoop();
-	
-	/// Get raw data from serial device
-	/// \param[out] bytes Number bytes in returned array
-	/// \return Raw data from the serial device
-	boost::shared_array<boost::uint8_t> GetRawSerialData(boost::uint32_t *bytes);
-	
-	/// Fill raw_buffer with new data
-	void FillRawBuffer();
+    /// Data points are captured  in this function. Function should be
+    /// called only by the capturing thread.
+    void CapturingLoop();
+    
+    /// Get raw data from serial device
+    /// \param[out] bytes Number bytes in returned array
+    /// \return Raw data from the serial device
+    boost::shared_array<boost::uint8_t> GetRawSerialData(boost::uint32_t *bytes);
+    
+    /// Fill raw_buffer with new data
+    void FillRawBuffer();
 
     size_t BytesWaiting();
  private:
-	
-	// Serial device string ("COM1", "/dev/ttyUSB0")
-	std::string device_string_;
-	
-	// Sampling rate in HZ
-	int hz_;
+    
+    // Serial device string ("COM1", "/dev/ttyUSB0")
+    std::string device_string_;
+    
+    // Sampling rate in HZ
+    int hz_;
 
-	// IO-service which was used to create the serial object
-	boost::asio::io_service io_service_;
-	
-	// Serial device object
-	boost::asio::serial_port device_;
-	
-	// Thread which captures the data from the serial object
+    // IO-service which was used to create the serial object
+    boost::asio::io_service io_service_;
+    
+    // Serial device object
+    boost::asio::serial_port device_;
+    
+    // Thread which captures the data from the serial object
     boost::thread acquisition_thread_;
-	
-	// If true, capturing will be stopped
-	bool stop_capturing_;
-	
-	// How long the capturing thread sleeps after it has processed all 
-	// data from the serial object
-	double capturing_thread_sleep_period_;
-	
-	// Container for captured data points
-	std::vector<ElementType> captured_values_;
-	
-	// Date of the first element of the 'captured_values_'
-	Date date_begin_;
-	
-	// Date of the last element of the 'captured_values_'
-	Date date_end_;
-	
-	// Mutex which protects the captured_values_
-	boost::mutex mutex_data_;
-	
-	// Raw data buffer to which all serial data is captured
-	std::vector<boost::uint8_t> raw_buffer_;
-	
+    
+    // If true, capturing will be stopped
+    bool stop_capturing_;
+    
+    // How long the capturing thread sleeps after it has processed all 
+    // data from the serial object
+    double capturing_thread_sleep_period_;
+    
+    // Container for captured data points
+    std::vector<ElementType> captured_values_;
+    
+    // Date of the first element of the 'captured_values_'
+    Date date_begin_;
+    
+    // Date of the last element of the 'captured_values_'
+    Date date_end_;
+    
+    // Mutex which protects the captured_values_
+    boost::mutex mutex_data_;
+    
+    // Raw data buffer to which all serial data is captured
+    std::vector<boost::uint8_t> raw_buffer_;
+    
 }; //class Daq
     
 } //namespace cdaq
